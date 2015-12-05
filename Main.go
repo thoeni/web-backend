1a0063f42345e04c988f16388b6f2404e9f728f4
Antonio Troina <antoniotroina@antonios-mbp.home>
2015-12-05 20:18:05 +0000

    Add db container IP lookup

--- Main.go
+++ Main.go
@@ -4,6 +4,8 @@ import (
 	"log"
 	"net/http"
 	"fmt"
+	"net"
+	"os"
 )
 
 const GOPORT string = "8080"
@@ -11,5 +13,11 @@ const GOPORT string = "8080"
 func main() {
 	router := NewRouter()
 	fmt.Println("Ready, listening on", GOPORT,"...")
+	var mariadb_container = os.Getenv("MARIADB_CONTAINER");
+	containerIp, err := net.LookupIP(mariadb_container)
+	if err != nil {
+		panic(err.Error()) // TODO proper error handling instead of panic in your app
+	}
+	fmt.Println("Database container IP is:", containerIp)
 	log.Fatal(http.ListenAndServe(":"+GOPORT, router))
 }
