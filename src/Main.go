75d09a651babf8877a62f80a847fd0dcf1de7dbc
Antonio Troina <antoniotroina@antonios-mbp.home>
2015-12-08 01:04:01 +0000

    Add CORS support

--- src/Main.go
+++ src/Main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 	"fmt"
+	"github.com/rs/cors"
 )
 
 const GOPORT string = "8080"
@@ -11,5 +12,5 @@ const GOPORT string = "8080"
 func main() {
 	router := NewRouter()
 	fmt.Println("Ready, listening on", GOPORT,"...")
-	log.Fatal(http.ListenAndServe(":"+GOPORT, router))
+	log.Fatal(http.ListenAndServe(":"+GOPORT, cors.Default().Handler(router)))
 }
